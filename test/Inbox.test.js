f666939d445d09a2306e0d52402bd0b2e02c5329
Jesus Fung <jesusfd988work@gmail.com>
2022-11-06 23:28:45 -0600

    removed the promise and now we use async (chapter 48)

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,14 +6,12 @@ const web3 = new Web3(ganache.provider()); // an instance - lower case
 // provider -> the one who joins/communicates ganache and web3. Tells web3 on what network to connect
 // in this case, to our local ganache
 
+let accounts;
 // beforeEach is only executed when there's at least 1 'it' test funct created
-beforeEach(() => {
+beforeEach(async () => {
     // get a list of all accounts
-    web3.eth.getAccounts().then(fetchedAccounts => {   
-        // fetchedAccounts = we get it from web3 lib which uses ganache local
-        // we use .then() because it is async.
-        console.log('fetched accounts: ' , fetchedAccounts) 
-    });
+    accounts = await web3.eth.getAccounts(); 
+    console.log("accounts: ", accounts);
     // use one of the accounts to deploy the contract
 })
 
